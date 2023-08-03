ff5ad69663a504cda60e561fe89df126eeb1d2d4
Kasun Abaywardana <abaywardanakasun@gmail.com>
2023-08-03 09:00:22 +0800

    Change the type of the component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import ChangeType from "../components/ChangeType";
 import ExtendingStyles from "../components/Extending";
 import General from "../components/General";
 import PropsStyled from "../components/Props";
@@ -5,9 +6,14 @@ import PropsStyled from "../components/Props";
 const HomePage: React.FC = () => {
     return (
         <>
+            <h3>General</h3>
             <General></General>
+            <h3>With Props</h3>
             <PropsStyled></PropsStyled>
+            <h3>Extending</h3>
             <ExtendingStyles></ExtendingStyles>
+            <h3>Change the original type</h3>
+            <ChangeType></ChangeType>
         </>
     )
 }
